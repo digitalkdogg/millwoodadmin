824feff1463cf3350a4f047718ef00599c8d6ddc
Kevin Bollman <digitalkdogg@gmail.com>
2021-05-01 10:16:04 -0500

    made db query sort by last updated

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <script type="application/javascript" src="assets/jQuery-3.4.1.js"></script>
     <script src="assets/bootstrap.min.js"></script>
     <script src = "assets/cookie.js"></script>
-    <script type="application/javascript" src = "assets/admin_js.js"></script>
+    <script type="application/javascript" src = "assets/adminjs.js"></script>
 
 </head>
 
